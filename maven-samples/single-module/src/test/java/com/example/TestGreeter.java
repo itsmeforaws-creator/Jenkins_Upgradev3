0b43f07b21820e7f192e28aaad579ff1f7721991
itsmeforaws-creator <itsmeforaws@gmail.com>
2026-06-05 10:27:46 +0530

    Update TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -26,7 +26,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
-  
+
+  @Test
+  public void failTest() {
+   assertTrue(false);
+  }
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
